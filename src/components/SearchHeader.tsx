cc669baa46cd3382db36f2a411cadfffc1608f12
Fevronia Gjini <fevronigjini1@gmail.com>
2022-07-26 02:17:00 +0200

    first commit

---
+++ src/components/SearchHeader.tsx
@@ -0,0 +1,11 @@
+          
+    function    SearchHeader() {
+         return( <div className="search">
+          <input className="search-bar" placeholder="Search mail" />
+        </div>
+
+        )
+        
+    }
+
+    export default SearchHeader
